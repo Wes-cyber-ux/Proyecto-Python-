f17001cc2dbd378d98b354c88dd29b1f6c089dd8
Wesley Laura <wesley.laura@utec.edu.pe>
2026-05-01 03:53:31 -0500

    Agregando los modulos principale del proyecto

---
+++ main.py
@@ -0,0 +1,12 @@
+from gui.ventana import iniciar_app  
+# Importa la función "iniciar_app" desde el archivo "ventana.py"
+# que está dentro de la carpeta "gui"
+
+if __name__ == "__main__":  
+# Esta condición verifica si este archivo se está ejecutando directamente
+# (por ejemplo: python main.py)
+# Si este archivo es importado desde otro, esto NO se ejecuta
+
+    iniciar_app()  
+    # Llama a la función "iniciar_app"
+    # Esto normalmente inicia la interfaz gráfica (abre la ventana)
