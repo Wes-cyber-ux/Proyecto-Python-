b9ba1fac356c3de89067738ce17ed151407c6eb3
Wesley Laura <wesley.laura@utec.edu.pe>
2026-05-04 21:20:24 -0500

    Agregando mas funciones

--- logic/funciones.py
+++ logic/funciones.py
@@ -1,2 +1,22 @@
 def saludar():
     print("BIENVENIDO AL PROYECTO DE PROGRAMACION I")
+
+def Eval_lanzamiento_por_clima():
+    return None
+
+def Ventana_de_lanzamiento_optima():
+    return None
+
+def Chequeo_probabilistico_de_sistemas():
+    return None
+
+def Cuenta_regresiva_con_probabilidad_de_falla():
+    return None
+
+def Validacion_de_sensores():
+    return None
+
+def Simulacion_de_combustible():
+    return None
+
+    
